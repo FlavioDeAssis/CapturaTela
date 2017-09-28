42affdaca26d90befc47fce6218930bc9a18ecec
SamExpress <suporte@sistemamotofrete.com.br>
2017-09-28 12:53:23 -0300

    Add project files.

---
+++ ConsultaEleitoral/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ConsultaEleitoral
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.btnRequest = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.fileSystemWatcher2 = new System.IO.FileSystemWatcher();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.txtNomeEleitor = new System.Windows.Forms.TextBox();
+            this.txtDataNascimento = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblDataNascimento = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(47, 31);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(553, 20);
+            this.urlTextBox.TabIndex = 0;
+            this.urlTextBox.Text = "http://apps.tse.jus.br/saae/consultaNomeDataNascimento.do";
+            // 
+            // btnRequest
+            // 
+            this.btnRequest.Location = new System.Drawing.Point(619, 30);
+            this.btnRequest.Name = "btnRequest";
+            this.btnRequest.Size = new System.Drawing.Size(113, 23);
+            this.btnRequest.TabIndex = 1;
+            this.btnRequest.Text = "&Acessar";
+            this.btnRequest.UseVisualStyleBackColor = true;
+            this.btnRequest.Click += new System.EventHandler(this.btnRequest_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // fileSystemWatcher2
+            // 
+            this.fileSystemWatcher2.EnableRaisingEvents = true;
+            this.fileSystemWatcher2.SynchronizingObject = this;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(47, 125);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(685, 274);
+            this.webBrowser.TabIndex = 2;
+            this.webBrowser.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(570, 690);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(162, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "&Salvar Consulta";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(47, 418);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(684, 230);
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // txtNomeEleitor
+            // 
+            this.txtNomeEleitor.Location = new System.Drawing.Point(47, 82);
+            this.txtNomeEleitor.Name = "txtNomeEleitor";
+            this.txtNomeEleitor.Size = new System.Drawing.Size(189, 20);
+            this.txtNomeEleitor.TabIndex = 5;
+            // 
+            // txtDataNascimento
+            // 
+            this.txtDataNascimento.Location = new System.Drawing.Point(300, 81);
+            this.txtDataNascimento.Name = "txtDataNascimento";
+            this.txtDataNascimento.Size = new System.Drawing.Size(175, 20);
+            this.txtDataNascimento.TabIndex = 6;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(47, 66);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(67, 13);
+            this.lblNome.TabIndex = 7;
+            this.lblNome.Text = "&Nome Eleitor";
+            // 
+            // lblDataNascimento
+            // 
+            this.lblDataNascimento.AutoSize = true;
+            this.lblDataNascimento.Location = new System.Drawing.Point(300, 66);
+            this.lblDataNascimento.Name = "lblDataNascimento";
+            this.lblDataNascimento.Size = new System.Drawing.Size(89, 13);
+            this.lblDataNascimento.TabIndex = 8;
+            this.lblDataNascimento.Text = "Data Nascimento";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 725);
+            this.Controls.Add(this.lblDataNascimento);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtDataNascimento);
+            this.Controls.Add(this.txtNomeEleitor);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.btnRequest);
+            this.Controls.Add(this.urlTextBox);
+            this.Name = "Form1";
+            this.Text = "Consulta Eleitoral";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button btnRequest;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.IO.FileSystemWatcher fileSystemWatcher2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label lblDataNascimento;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtDataNascimento;
+        private System.Windows.Forms.TextBox txtNomeEleitor;
+    }
+}
+
---
+++ ConsultaEleitoral/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ConsultaEleitoral
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {   
+            // incializa componetes
+            InitializeComponent();
+        }
+        
+        // inicia requisisição do conteudo do site utilizando class WebBrowser 
+        private void btnRequest_Click(object sender, EventArgs e)
+        {
+            Navigate(urlTextBox.Text);
+        }
+
+        // tratamento da url inserida na box de UL
+        private void Navigate(String address)
+        {
+            if (String.IsNullOrEmpty(address)) return;
+            if (address.Equals("about:blank")) return;
+            if (!address.StartsWith("http://") &&
+                !address.StartsWith("https://"))
+            {
+                address = "http://" + address;
+            }
+            try
+            {
+                webBrowser.Navigate(new Uri(address));
+            }
+            catch (System.UriFormatException)
+            {
+                return;
+            }
+        }
+
+        // gerar a captura de imagem. salva imagem e arquivo
+        // obs: alterar letra do diretorio de destino
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // captura imagem
+            pictureBox.Image = Tela.RetornaImagemControle(this.Handle, new Rectangle(0, 0, this.Width, this.Height));
+            // salva imagem
+            pictureBox.Image.Save(@"e:\teste.bmp", ImageFormat.Bmp);
+
+            // gerar arquivo word
+            // Referencia: http://www.andrealveslima.com.br/blog/index.php/2016/06/29/gerando-arquivos-do-word-com-c-e-vb-net/
+            // obs: talvez seja necessario obter o pacote atraves do gerenciado 
+            var wordApp = new Microsoft.Office.Interop.Word.Application();
+            wordApp.Visible = false;
+            var wordDoc = wordApp.Documents.Add();
+            
+            // adicionando imagem ao arquivo
+            var paragrafo5 = wordDoc.Paragraphs.Add();
+            paragrafo5.Range.InlineShapes.AddPicture(@"e:\teste.bmp");
+
+            // salva arquivo
+            wordDoc.SaveAs2(@"e:\" + txtNomeEleitor.Text + "_" + txtDataNascimento.Text +".docx");
+            wordApp.DisplayAlerts = Microsoft.Office.Interop.Word.WdAlertLevel.wdAlertsNone;
+            wordApp.Quit();
+        }
+        
+    }
+
+    // classe para gerar imagem
+    //Referencia: https://msdn.microsoft.com/pt-br/library/dn630211.aspx
+    public class Tela
+    {
+        [DllImport("user32.dll", EntryPoint = "GetDC")]
+        static extern IntPtr GetDC(IntPtr ptr);
+        [DllImport("user32.dll", EntryPoint = "ReleaseDC")]
+        static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDc);
+        [DllImport("gdi32.dll", EntryPoint = "DeleteDC")]
+        static extern IntPtr DeleteDC(IntPtr hDc);
+        [DllImport("gdi32.dll", EntryPoint = "CreateCompatibleDC")]
+        static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+        [DllImport("gdi32.dll", EntryPoint = "CreateCompatibleBitmap")]
+        static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
+        [DllImport("gdi32.dll", EntryPoint = "SelectObject")]
+        static extern IntPtr SelectObject(IntPtr hdc, IntPtr bmp);
+        [DllImport("gdi32.dll", EntryPoint = "BitBlt")]
+        static extern bool BitBlt(IntPtr hdcDest, int xDest, int yDest, int wDest, int hDest, IntPtr hdcSource, int xSrc, int ySrc, int RasterOp);
+        [DllImport("user32.dll", EntryPoint = "GetDesktopWindow")]
+        static extern IntPtr GetDesktopWindow();
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        static extern IntPtr DeleteObject(IntPtr hDc);
+
+        const int SRCCOPY = 13369376;
+
+        public struct SIZE
+        {
+            public int cx;
+            public int cy;
+            
+        }
+
+        public static Bitmap RetornaImagemControle(IntPtr controle, Rectangle area)
+        {
+            SIZE size;
+            IntPtr hBitmap;
+
+            IntPtr hDC = GetDC(controle);
+            IntPtr hMemDC = CreateCompatibleDC(hDC);
+
+            size.cx = area.Width - area.Left;
+            size.cy = area.Bottom - area.Top;
+
+            hBitmap = CreateCompatibleBitmap(hDC, size.cx, size.cy);
+
+            if (hBitmap != IntPtr.Zero)
+            {
+                IntPtr hOld = (IntPtr)SelectObject(hMemDC, hBitmap);
+                BitBlt(hMemDC, 0, 0, size.cx, size.cy, hDC, 0, 0, SRCCOPY);
+                SelectObject(hMemDC, hOld);
+                DeleteDC(hMemDC);
+                ReleaseDC(GetDesktopWindow(), hDC);
+                Bitmap bmp = System.Drawing.Image.FromHbitmap(hBitmap);
+                DeleteObject(hBitmap);
+                return bmp;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
